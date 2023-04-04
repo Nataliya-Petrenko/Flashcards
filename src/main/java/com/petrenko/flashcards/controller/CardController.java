08c82f2b4bb908eab094a6fdad7e4c0c17e3feb9
Nataliya <nataliya_petrenko_@ukr.net>
2023-04-04 07:27:55 +0300

    Correcting filling the form for a card (not finished)

--- src/main/java/com/petrenko/flashcards/controller/CardController.java
+++ src/main/java/com/petrenko/flashcards/controller/CardController.java
@@ -1,8 +1,6 @@
 package com.petrenko.flashcards.controller;
 
-import com.petrenko.flashcards.model.Card;
-import com.petrenko.flashcards.model.KeyWord;
-import com.petrenko.flashcards.model.SetOfCards;
+import com.petrenko.flashcards.model.*;
 import com.petrenko.flashcards.service.CardService;
 import com.petrenko.flashcards.service.KeyWordService;
 import com.petrenko.flashcards.service.SetOfCardsService;
@@ -14,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -80,12 +79,31 @@ public class CardController {
 //        return modelAndView;
 //    }
 
-    @PostMapping("/create")
+    @GetMapping("/create")
+    public ModelAndView getArticleForm(ModelAndView modelAndView) {
+        Card card = new Card();
+        modelAndView.addObject("card", card);
+
+        List<String> studyPriorities = Arrays.stream(StudyPriority.values())
+                .map(Enum::toString)
+                .toList();
+        modelAndView.addObject("priorities", studyPriorities);
+
+        List<String> knowledgeLevels = Arrays.stream(KnowledgeLevel.values())
+                .map(Enum::toString)
+                .toList();
+        modelAndView.addObject("knowledgeLevels", knowledgeLevels);
+
+        modelAndView.setViewName("editCardView");
+        return modelAndView;
+    }
+
+    @PostMapping
     public ModelAndView saveNewCard(@ModelAttribute @Valid Card card, BindingResult bindingResult,
                                     ModelAndView modelAndView) {
         if (bindingResult.hasErrors()) {
             modelAndView.addObject("card", card);
-            modelAndView.setViewName("cardFormView");
+            modelAndView.setViewName("editCardView.html");
             return modelAndView;
         }
         cardService.save(card);
@@ -94,28 +112,42 @@ public class CardController {
         return modelAndView;
     }
 
-    @PutMapping("/edit")  // todo & ("/{id}") ?
-    public ModelAndView updateCard(@ModelAttribute @Valid Card card, BindingResult bindingResult,
-                                   ModelAndView modelAndView) {
-        if (bindingResult.hasErrors()) {
-            modelAndView.addObject("card", card);
-            modelAndView.setViewName("cardFormView");
-            return modelAndView;
-        }
-        cardService.save(card);  // todo check correct work for update
-        System.out.println(card);
-        modelAndView.setViewName("cardView");
-        return modelAndView;
-    }
+//    @PostMapping("/create")
+//    public ModelAndView saveNewCard(@ModelAttribute @Valid Card card, BindingResult bindingResult,
+//                                    ModelAndView modelAndView) {
+//        if (bindingResult.hasErrors()) {
+//            modelAndView.addObject("card", card);
+//            modelAndView.setViewName("editCardView.html");
+//            return modelAndView;
+//        }
+//        cardService.save(card);
+//        System.out.println(card);
+//        modelAndView.setViewName("cardView");
+//        return modelAndView;
+//    }
 
-    @DeleteMapping("/{id}")
-    public void deleteCard(@PathVariable String id) {
-        if (id != null) {
-            cardService.deleteById(id);
-        }
-
-        // go to set
-    }
+//    @PutMapping("/edit")  // todo & ("/{id}") ?
+//    public ModelAndView updateCard(@ModelAttribute @Valid Card card, BindingResult bindingResult,
+//                                   ModelAndView modelAndView) {
+//        if (bindingResult.hasErrors()) {
+//            modelAndView.addObject("card", card);
+//            modelAndView.setViewName("cardFormView");
+//            return modelAndView;
+//        }
+//        cardService.save(card);  // todo check correct work for update
+//        System.out.println(card);
+//        modelAndView.setViewName("cardView");
+//        return modelAndView;
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void deleteCard(@PathVariable String id) {
+//        if (id != null) {
+//            cardService.deleteById(id);
+//        }
+//
+//        // go to set
+//    }
 
 
 }

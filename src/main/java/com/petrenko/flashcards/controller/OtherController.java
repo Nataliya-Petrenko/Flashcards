35bbbdc11faa1d6cb105d6dca6dddc140806c3e8
Nataliya <nataliya_petrenko_@ukr.net>
2023-04-16 18:09:37 +0300

    Learn in the set is available if a list card is not empty

--- src/main/java/com/petrenko/flashcards/controller/OtherController.java
+++ src/main/java/com/petrenko/flashcards/controller/OtherController.java
@@ -2,14 +2,14 @@ package com.petrenko.flashcards.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-
-@org.springframework.stereotype.Controller
+@Controller
 @RequestMapping()
-public class Controller {
-    private final static Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+public class OtherController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(OtherController.class);
 
     @GetMapping("/info")
     public ModelAndView getInfo(ModelAndView modelAndView) {
@@ -17,11 +17,5 @@ public class Controller {
         modelAndView.setViewName("info");
         return modelAndView;
     }
-
-    // todo check final
-    // check cyclic reps
-    // checking null in controller
-    // checking logic in services
-
 
 }

9131bd9f5e61b55debbf54ff0f71e772984c86e6
Nataliya <nataliya_petrenko_@ukr.net>
2023-04-25 18:01:30 +0300

    Migration

---
+++ src/main/resources/db.migration/V3_0__insert_persons.sql
@@ -0,0 +1,18 @@
+insert into person (id, email, enable, first_name, last_name, password, role)
+values ('09274bc9-dca5-4144-9d18-556c055524b9', 'vShnSw95@HuDX.RTC', 'true', 'aQqvj', 'iFfmTfoo', '$2a$10$ZSFyx0i0ZAaW1KPYk3.tCuPAGhZKn0Amy6/M03eNdlnASqzQFCHMC', 'USER');
+
+
+insert into person (id, email, enable, first_name, last_name, password, role)
+values ('93964704-3edc-4db0-8223-9a53b15d9245', 'DdxCBv27@KFAs.foN', 'false', 'QBRFK', 'XOJbjrwT', '$2a$10$ZUmuuISgfI878iTNoNyh2.wJYf2NyrnlprhUyTmKpGkU51ocM1N4.', 'USER');
+
+
+insert into person (id, email, enable, first_name, last_name, password, role)
+values ('eef2442f-ad19-4616-a11c-44a04f7c2f87', 'eOYjMR28@YbqX.Xwe', 'false', 'XJaxk', 'XHeloudp', '$2a$10$W/LO9eRXsJ.Wq8fp.9asjeolMa4WrGq6C8a7VB9poA8Ecq61XQKsq', 'ADMIN');
+
+
+insert into person (id, email, enable, first_name, last_name, password, role)
+values ('f0509925-ffcc-423f-bb9b-9f71c55f9ece', 'HjoxHx37@plmR.eUM', 'false', 'SiUqX', 'CVJEtwjD', '$2a$10$LbZso7tHxdDhLgyd93RNZuH4F7p4FRHTmkBSDWUpLx2DPP.oOwHam', 'USER');
+
+
+insert into person (id, email, enable, first_name, last_name, password, role)
+values ('59a93155-e34b-491f-a1e4-94d2283b3afa', 'aKqKjM57@EzpR.Jfn', 'false', 'GTuxn', 'JBnJwRKO', '$2a$10$OmNdc9lSUoT7u.xE4TVRKutiVaNbhiYK4bEeK.c2DkYSElA988T9i', 'USER');
